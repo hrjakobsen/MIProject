02b7b7a3497e5f0b21323f9c69a0415d5dbdb9e8
Mikkel Kettunen <mikkel.kettunen@gmail.com>
2017-11-20 15:16:00 +0100

    Fixed Mathias buggy code :100:

--- project/games/pong.py
+++ project/games/pong.py
@@ -6,6 +6,7 @@ UP = 1
 NOTHING = 0
 DOWN = -1
 
+
 class PongGame(object):
     def __init__(self):
         self.width = 500
@@ -19,7 +20,7 @@ class PongGame(object):
         else:
             direction += 45
 
-        velocity = [math.cos(direction * math.pi/180), math.sin(direction * math.pi/180)]
+        velocity = [math.cos(direction * math.pi / 180), math.sin(direction * math.pi / 180)]
         self.ballVelocity = np.array(velocity)
         self.ballPosition = np.array([self.width // 2, self.height // 2])
         self.ballRadius = 2.5
@@ -47,6 +48,7 @@ class PongGame(object):
         new.ballPosition = self.ballPosition
         return new
 
+
 def updateBall(state: PongGame):
     newPos = state.ballPosition + state.ballVelocity
     newVel = state.ballVelocity
@@ -57,7 +59,7 @@ def updateBall(state: PongGame):
         return newPos, state.ballVelocity, None
 
     if factorWall < 1 and factorPaddle < 1:
-        #Update the smallest factor first
+        # Update the smallest factor first
         if factorWall < factorPaddle:
             newPos = state.ballPosition + state.ballVelocity * factorWall
             newVel[1] *= -1
@@ -78,6 +80,21 @@ def updateBall(state: PongGame):
                 remainingFactor = 1 - (factorPaddle + factorWall)
                 newPos += newVel * remainingFactor
                 return newPos, newVel, int(newPos[0] < state.width // 2) + 1
+        else:
+            newPos = state.ballPosition + state.ballVelocity * factorPaddle
+            paddlePos = state.p1pos if (newPos[0] < state.width / 2) else state.p2pos
+            if paddlePos - state.paddleHeight // 2 < newPos[1] < paddlePos + state.paddleHeight // 2:
+                # Bounced!
+                newVel[0] *= -1
+                newPos += state.ballVelocity * (factorPaddle - factorWall)
+                newVel[1] *= -1
+                newPos += state.ballVelocity * (1 - factorPaddle - factorWall)
+            else:
+                # Someone lost
+                remainingFactor = 1 - factorPaddle
+                newPos += newVel * remainingFactor
+                return newPos, newVel, int(newPos[0] < state.width // 2) + 1
+
     elif factorWall < 1:
         newPos = state.ballPosition + state.ballVelocity * factorWall
         newVel = state.ballVelocity * np.array([1, -1])
@@ -98,6 +115,7 @@ def updateBall(state: PongGame):
 
     return newPos, newVel, None
 
+
 def factorToWall(state: PongGame):
     """
     this function returns a factor that multiplied to the velocity vector results in
@@ -105,7 +123,11 @@ def factorToWall(state: PongGame):
     :param state:
     :return:
     """
-    return max([(wall - state.ballPosition[1])/state.ballVelocity[1] for wall in [state.ballRadius, state.height - state.ballRadius]])
+    if state.ballVelocity[1] == 0:
+        return 1e20  # a large value
+    return max([(wall - state.ballPosition[1]) / state.ballVelocity[1] for wall in
+                [state.ballRadius, state.height - state.ballRadius]])
+
 
 def factorToPaddle(state: PongGame):
     """
@@ -127,6 +149,7 @@ def makeMove(state: PongGame, action1, action2):
 
     return newState
 
+
 def paddleUpdate(state, action, player):
     myPos = state.p1pos if player == 1 else state.p2pos
     return max(min(state.height - state.paddleHeight // 2, myPos + state.paddleSpeed * action), state.paddleHeight // 2)
@@ -136,31 +159,36 @@ def getFeatures(player):
     features = [
         lambda s, a: 1,
         # the position of the paddle after a move
-        lambda s, a: paddlePositions(s, a, player)[1],
+        # lambda s, a: paddlePositions(s, a, player)[1],
         # the position of the ball after a move
-        lambda s, a: makePlayerMove(s, a, player).ballPosition[0],
-        lambda s, a: makePlayerMove(s, a, player).ballPosition[1],
+        # lambda s, a: makePlayerMove(s, a, player).ballPosition[0],
+        # lambda s, a: makePlayerMove(s, a, player).ballPosition[1],
         # the velocity of the ball after a move
-        lambda s, a: makePlayerMove(s, a, player).ballVelocity[0],
-        lambda s, a: makePlayerMove(s, a, player).ballVelocity[1],
-        lambda s, a: distanceToBall(s, a, player)
+        # lambda s, a: makePlayerMove(s, a, player).ballVelocity[0],
+        # ambda s, a: makePlayerMove(s, a, player).ballVelocity[1],
+
+        lambda s, a: distanceToBall(s, a, player),
     ]
 
     return features
+
 
 def distanceToBall(s, a, player):
     s2 = makePlayerMove(s, a, player)
     paddleY = s2.p1pos if player == 1 else s2.p2pos
     return math.log2(1 / max(((s2.ballPosition[1] - paddleY) ** 2), 0.00001))
 
+
 def makePlayerMove(s, a, player):
     return makeMove(s, a, NOTHING) if player == 1 else makeMove(s, NOTHING, a)
+
 
 def getReward(state: PongGame, player):
     if state.winner is None:
         return 0
     # 1 for winning and -1 for losing
     return 1 if player == state.winner else -1
+
 
 def paddlePositions(state: PongGame, action, player):
     enemyPos = state.p2pos if player == 1 else state.p1pos
